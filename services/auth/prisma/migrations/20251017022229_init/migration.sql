761b45916bc4bfe4272167592e045b3eafb8e8f4
wolfiee42 <saifalislam2022@gmail.com>
2025-10-17 08:23:06 +0600

    feat: implement user authentication and authorization in user service

---
+++ services/auth/prisma/migrations/20251017022229_init/migration.sql
@@ -0,0 +1,64 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'USER');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('PENDING', 'ACTIVE', 'INACTIVE', 'BLOCKED');
+
+-- CreateEnum
+CREATE TYPE "LoginAttempt" AS ENUM ('SUCCESS', 'FAILED');
+
+-- CreateEnum
+CREATE TYPE "VerificationStatus" AS ENUM ('PENDING', 'VERIFIED', 'EXPIRED');
+
+-- CreateEnum
+CREATE TYPE "VerificationType" AS ENUM ('ACCOUNT_VERIFICATION', 'PASSWORD_RESET', 'EMAIL_CHANGE', 'PHONE_CHANGE', 'TWO_FACTOR_AUTH', 'TWO_FACTOR_AUTH_DISABLE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "status" "UserStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "LoginHistory" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "ipAdress" TEXT NOT NULL,
+    "userAgent" TEXT NOT NULL,
+    "attempt" "LoginAttempt" NOT NULL DEFAULT 'SUCCESS',
+    "loginAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "LoginHistory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationCode" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "status" "VerificationStatus" NOT NULL DEFAULT 'PENDING',
+    "type" "VerificationType" NOT NULL DEFAULT 'ACCOUNT_VERIFICATION',
+    "issuedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "verifiedAt" TIMESTAMP(3),
+
+    CONSTRAINT "VerificationCode_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "LoginHistory" ADD CONSTRAINT "LoginHistory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VerificationCode" ADD CONSTRAINT "VerificationCode_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ services/auth/src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+
+app.get("/health", (_req, res) => {
+    res.status(200).json({ status: "UP" });
+});
+
+
+//routes
+
+
+
+// 404 handler
+app.use((_req, res) => {
+    res.status(404).json({ error: "Not Found" });
+});
+
+// error handler
+app.use((err, _req, res, _next) => {
+    console.log(err.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+});
+
+const port = process.env.PORT || 4003;
+const servicename = process.env.SERVICE_NAME || "auth-service";
+
+app.listen(port, () => {
+    console.log(`${servicename} is running on port ${port}`);
+});
